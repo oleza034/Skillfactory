5f6a54deb18b7631a1b3fa939be33e9a94593601
Oleg Zakaznov <oleza034@yandex.ru>
2022-08-06 19:10:17 +0300

    Test challenge 1, complete

--- Python/11.3 First project/Test challenge1.py
+++ Python/11.3 First project/Test challenge1.py
@@ -14,18 +14,6 @@ def test(txt: str):
     submit.click()
 
 
-def test1(txt: str):
-    """Special test 1: double click submit button"""
-    time.sleep(1)
-
-    el = web.find_element(By.XPATH, "//*[@id=\"firstname\"]")
-    el.send_keys(txt)
-
-    submit = web.find_element(By.XPATH, "//*[@name=\"formSubmit\"]")
-    submit.click()
-    submit.click()
-
-
 web = webdriver.Chrome()
 web.get('http://testingchallenges.thetestingmap.org/index.php')
 
@@ -46,14 +34,48 @@ test('qwertyuiopasdfghjklzxcvbnmpoiu')
 
 # More than maximum length
 test('qwertyuiopasdfghjklzxcvbnmpoiui')
+
+# space
 test(' ')
+
+# Space in the middle
 test('User 2')
-test(u'юзверь')
+
+# Space in the end
 test('user1 ')
+
+# Space in the beginning
 test(' User')
 
-# this does not result to a new type of test but should be tested
-test1('User')
+# Non-ASCII value
+test(u'юзверь')
+
+# HTML tags
+test('<i>Mighty user</i>')
+
+# Basic SQL injection
+test('test\'test;')
+
+# Basic XSS
+test('<script></script>')
+
+# Missing CSS link
+test('detailsoverviewnow.css')
+
+# Looked at the page source
+test('dfjwGGe82H43g3uRiy53h')
+
+# Test the cookie
+test('oi32jnxd42390slk345')
+
+# Test admin rights
+adm = web.find_element(By.XPATH, '//*[@name=\'user_right_as_admin\']')
+web.execute_script('''
+    var elem = arguments[0];
+    var value = arguments[1];
+    elem.value = value;
+''', adm, '1')
+test('admin')
 
 time.sleep(10)
 web.close()
