80facfa5665d6f4451d9ed78b5ee3b6ac21b6e94
Oleg Zakaznov <oleza034@yandex.ru>
2022-08-16 14:41:35 +0300

    13.8.19 Ticket price calculation

--- Python/pythonProject/main.py
+++ Python/pythonProject/main.py
@@ -1,3 +1,14 @@
-s = '/home/user/documents/file.txt'
+import numpy
 
-print(s.split('/')[-2])
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+
+money = int(input('Введите сумму:'))
+
+
+deposit = list(per_cent.values())
+
+deposit = deposit * money
+#for i in deposit:
+#    deposit[i] *= money
+
+print(deposit)
