fd23c35b8ca335044052766f8fc9a4cb014ad35c
Oleg Zakaznov <oleza034@yandex.ru>
2022-08-30 18:19:07 +0300

    updating calculator script

--- Python/13.8.19 Ticket price calculation/main.py
+++ Python/13.8.19 Ticket price calculation/main.py
@@ -21,20 +21,41 @@ def price(guest_age: int):
     :return: price applied for the guest of his or her age
     '''
     # declare prices dict as age limit and its price. For example, {0: 0} means a guest must be not younger than 0 years
-    prices = {(0, 18): 0, (18, 25): 990, (25, 200): 1390}
+    prices = {
+        (0, 18): 0, # 1st age from 0 to 18: for free
+        (18, 25): 990, # 2nd age from 18 to 25: discount
+        (25, 200): 1390 # 3rd age older than 25: full price
+    }
+    
+    price = None
 
-    return max([p for a, p in zip(prices.keys(), prices.values()) if a[0] <= guest_age < a[1]])
+    for a in prices.keys():
+        if a[0] <= guest_age < a[1]:
+            price = prices[a]
+
+    return price
 
 
 # order totals
-def totals(guests_list: [], discount=False):
+def totals(guests_list: list, return_discount=False):
     '''
     Returns total price for the order
     :param guests_list: list of guest ages
     :param discount: True if you want to get just discount; False for order totals
     :return: total price for the order or the sum of discount value, if requested
     '''
-    return sum((.1 if discount else (.9 if len(guests_list) > 3 else 1)) * p for p in map(price, guests_list))
+    total_sum = 0
+    discount = len(guests_list) > 3
+    
+    for a in guests_list: # take all guests ages
+        if discount and return_discount:
+            total_sum += price(a) * 0.1
+        elif discount:
+            total_sum += price(a) * 0.9
+        elif not return_discount:
+            total_sum += price(a)
+            
+    return total_sum
 
 
 n = input("Введите количество пользователей: ")
@@ -56,7 +77,7 @@ for i in range(1, n + 1):
         age = input(("Неверный ввод. " + ("Введите возраст меньше 200 лет" if age.isdigit() else "Введите число")
             + ". Повторите ввод возраста {}-го пользователя: ").format(i))
 
-    print('user\'s price: ', price(int(age)))
+    # print('user\'s price: ', price(int(age)))
     L.append(int(age))
 
 print('')
