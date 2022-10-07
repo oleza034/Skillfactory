f8fc995077764a47cd4573efc2afab23dbdb652d
Oleg Zakaznov <oleza034@yandex.ru>
2022-10-07 11:53:38 +0300

    update app

--- Python/19 PyTest and REST APT tests/PetFriends/settings.py
+++ Python/19 PyTest and REST APT tests/PetFriends/settings.py
@@ -7,4 +7,4 @@ load_dotenv()
 valid_email = os.getenv('valid_email')
 valid_password = os.getenv('valid_password')
 # redis_connection = json.loads(os.getenv('redis_connection')) # dict with server, port and password
-project_folder = os.getenv('project_folder')
+# project_folder = os.getenv('project_folder')
